8556a00afaf0254c99dd31943708be093fd96a21
Ilya Kalashnikov <kalasxel@gmail.com>
2016-07-30 23:16:56 +0300

    avfad

--- tes.cpp
+++ tes.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char** argv)
 {{
-  std::cout << "Hello, World of Git!" << std::endl;
+  cout << "Hello, World of Git!" << endl;
   return 0;
 }}
